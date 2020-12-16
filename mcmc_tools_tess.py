5507c1f0c9e01f24ba85feadc63a1acdac877ee9
kdettman <dettman@physics.rutgers.edu>
2020-12-16 16:11:58 -0500

    Added multiple models, updated models

--- mcmc_tools_tess.py
+++ mcmc_tools_tess.py
@@ -43,7 +43,7 @@ def log_prior(theta):
     if sigma <= 0:
         return -np.inf
 
-    if (power < 1.0) or (power > 3.0):
+    if (power < 0.5) or (power > 5.0):
         return -np.inf
 
     if np.abs(t0) > 10:
@@ -55,33 +55,125 @@ def log_prior(theta):
 
     return 0.
 
-def log_likelihood(theta, data):
+def model_0(data, t0, a, power, offset):
+    ret = np.zeros(len(data))
+    ret[data-t0 > 0] = (a) * (data[data-t0 > 0] - t0)**power
+    ret+=offset
+    return ret
+
+def model_1(data, t0, a, power, offset):
+    ret = np.zeros(len(data))
+    ret[data-t0 > 0] = (a**power) * (data[data-t0 > 0] - t0)**power
+    ret+=offset
+    return ret
+
+def model_2(data, t0, a, power, offset):
+    ret = np.zeros(len(data))
+    ret[data-t0 > 0] = (a**(1./power)) * (data[data-t0 > 0] - t0)**power
+    ret+=offset
+    return ret
+
+def model_3(data, t0, a, power, offset):
+    ret = np.zeros(len(data))
+    ret[data-t0 > 0] = (a**(power/3.0)) * (data[data-t0 > 0] - t0)**power
+    ret+=offset
+    return ret
+
+def log_likelihood(theta, data, model):
     
     t0, a, sigma, power, offset = get_fullparam(theta)
 
     # power = 2.0
     
     var = (data['flux_err']**2 + sigma**2)
-    # model = np.heaviside(data['mjd'] - t0, 0) * a * (data['mjd'] - t0)**power
+    
     model = np.zeros(len(data))
-    model[data['mjd']-t0 > 0] = a * (data['mjd'][data['mjd']-t0 > 0] - t0)**power
+    model[data['mjd']-t0 > 0] = (a**power) * (data['mjd'][data['mjd']-t0 > 0] - t0)**power
     model+=offset
+
     logl = -0.5 * (np.sum(np.log(2 * np.pi * var) + 
                             ((data['flux'] - model)**2 / var) ))
         
     return logl
 
+def log_likelihood_0(theta, data):
+    t0, a, sigma, power, offset = get_fullparam(theta)
+    var = (data['flux_err']**2 + sigma**2)
+    mod = model_0(data['mjd'], t0, a, power, offset)
+
+    logl = -0.5 * (np.sum(np.log(2 * np.pi * var) + 
+                            ((data['flux'] - mod)**2 / var) ))
+        
+    return logl
+
+def log_likelihood_1(theta, data):
+    t0, a, sigma, power, offset = get_fullparam(theta)
+    var = (data['flux_err']**2 + sigma**2)
+    mod = model_1(data['mjd'], t0, a, power, offset)
+
+    logl = -0.5 * (np.sum(np.log(2 * np.pi * var) + 
+                            ((data['flux'] - mod)**2 / var) ))
+        
+    return logl
+
+def log_likelihood_2(theta, data):
+    t0, a, sigma, power, offset = get_fullparam(theta)
+    var = (data['flux_err']**2 + sigma**2)
+    mod = model_2(data['mjd'], t0, a, power, offset)
+
+    logl = -0.5 * (np.sum(np.log(2 * np.pi * var) + 
+                            ((data['flux'] - mod)**2 / var) ))
+        
+    return logl
+
+def log_likelihood_3(theta, data):
+    t0, a, sigma, power, offset = get_fullparam(theta)
+    var = (data['flux_err']**2 + sigma**2)
+    mod = model_3(data['mjd'], t0, a, power, offset)
+
+    logl = -0.5 * (np.sum(np.log(2 * np.pi * var) + 
+                            ((data['flux'] - mod)**2 / var) ))
+        
+    return logl
+
     
-def log_posterior(theta, data):
+def log_posterior_0(theta, data):
     
     logpr = log_prior(theta)
     
     if logpr == -np.inf:
         return logpr
     else:
-        return logpr + log_likelihood(theta, data)
+        return logpr + log_likelihood_0(theta, data)
 
-def doMCMC(data, guess, scale, nwalkers=100, nburn=1500, nsteps=3000):
+def log_posterior_1(theta, data):
+    
+    logpr = log_prior(theta)
+    
+    if logpr == -np.inf:
+        return logpr
+    else:
+        return logpr + log_likelihood_1(theta, data)
+
+def log_posterior_2(theta, data):
+    
+    logpr = log_prior(theta)
+    
+    if logpr == -np.inf:
+        return logpr
+    else:
+        return logpr + log_likelihood_2(theta, data)
+
+def log_posterior_3(theta, data):
+    
+    logpr = log_prior(theta)
+    
+    if logpr == -np.inf:
+        return logpr
+    else:
+        return logpr + log_likelihood_3(theta, data)
+
+def doMCMC(data, guess, scale, model, nwalkers=100, nburn=1500, nsteps=3000):
     '''
     Takes data which contains mjd and flux data
     and performs an mcmc fit on it
@@ -91,8 +183,17 @@ def doMCMC(data, guess, scale, nwalkers=100, nburn=1500, nsteps=3000):
 
     starting_guesses = np.random.randn(nwalkers, ndim)*scale + guess
 
+    if model == 'model 0':
+        log_post = log_posterior_0
+    elif model == 'model 1':
+        log_post = log_posterior_1
+    elif model == 'model 2':
+        log_post = log_posterior_2
+    elif model == 'model 3':
+        log_post = log_posterior_3
+
     print('sampling...')
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, threads=1, args=[data])
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_post, threads=1, args=[data])
     sampler.run_mcmc(starting_guesses, nsteps, progress=True)
     print('done')
     
